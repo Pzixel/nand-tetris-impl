838a09f8479d539bac0ce4753f4d6b8ebc06479d
Pzixel <pzixel@gmail.com>
2024-10-07 08:33:03 +0300

    add variables

--- Assembler/src/main.rs
+++ Assembler/src/main.rs
@@ -1,6 +1,7 @@
 use core::fmt;
 use std::{env, fmt::Display, str::FromStr};
 
+#[derive(Debug)]
 struct Dest {
     a: bool,
     m: bool,
@@ -112,8 +113,26 @@ impl FromStr for Comp {
     }
 }
 
+#[derive(Debug)]
 enum Command {
     A(u16),
+    C {
+        comp: Comp,
+        dest: Dest,
+        jump: Jump,
+    }
+}
+
+#[derive(Debug)]
+enum Address {
+    Value(u16),
+    Variable(String),
+}
+
+#[derive(Debug)]
+enum CodeLine {
+    Label(String),
+    A(Address),
     C {
         comp: Comp,
         dest: Dest,
@@ -144,54 +163,152 @@ impl From<&Command> for Instruction {
     }
 }
 
-fn assemble(content: &str) -> Vec<Instruction> {
-    let commands = parse_file(content);
-    commands.iter().map(|c| Instruction::from(c)).collect()
+#[derive(Debug)]
+struct SymbolTable {
+    symbols: std::collections::HashMap<String, u16>,
+    next_address: u16,
 }
 
-fn parse_file(content: &str) -> Vec<Command> {
-    content.lines()
-        .map(|line| {
-            if let Some(comment_idx) = line.find("//") {
-                line[..comment_idx].trim()
-            } else {
-                line.trim()
-            }
+impl SymbolTable {
+    fn get_or_insert(&mut self, variable: String) -> u16 {
+        *self.symbols.entry(variable).or_insert_with(|| {
+            let address = self.next_address;
+            self.next_address += 1;
+            address
         })
-        .filter(|x| !x.is_empty())
-        .map(parse_line)
-        .collect()
+    }
+
+    fn insert(&mut self, symbol: String, address: u16) {
+        // dbg!(&self, &symbol, &address);
+        if let Some(x) = self.symbols.insert(symbol.clone(), address) {
+            panic!("Symbol {} already exists with address {}", symbol, x);
+        }
+    }
 }
 
-fn parse_line(line: &str) -> Command {
-    if line.as_bytes()[0] == b'@' {
-        Command::A(line[1..].parse().expect("Invalid A command"))
-    } else {
-        let mut dest = Dest { a: false, m: false, d: false };
-        let mut jump = Jump::Null;
-        let mut comp = &line[..];
-        if let Some(idx) = line.find('=') {
-            let (d, c) = line.split_at(idx);
-            for c in d.chars() {
-                match c {
-                    'A' => dest.a = true,
-                    'M' => dest.m = true,
-                    'D' => dest.d = true,
-                    _ => panic!("Invalid dest {}", d)
+impl Default for SymbolTable {
+    fn default() -> Self {
+        let mut symbols = std::collections::HashMap::new();
+        symbols.insert("SP".to_string(), 0);
+        symbols.insert("LCL".to_string(), 1);
+        symbols.insert("ARG".to_string(), 2);
+        symbols.insert("THIS".to_string(), 3);
+        symbols.insert("THAT".to_string(), 4);
+        symbols.insert("R0".to_string(), 0);
+        symbols.insert("R1".to_string(), 1);
+        symbols.insert("R2".to_string(), 2);
+        symbols.insert("R3".to_string(), 3);
+        symbols.insert("R4".to_string(), 4);
+        symbols.insert("R5".to_string(), 5);
+        symbols.insert("R6".to_string(), 6);
+        symbols.insert("R7".to_string(), 7);
+        symbols.insert("R8".to_string(), 8);
+        symbols.insert("R9".to_string(), 9);
+        symbols.insert("R10".to_string(), 10);
+        symbols.insert("R11".to_string(), 11);
+        symbols.insert("R12".to_string(), 12);
+        symbols.insert("R13".to_string(), 13);
+        symbols.insert("R14".to_string(), 14);
+        symbols.insert("R15".to_string(), 15);
+        symbols.insert("SCREEN".to_string(), 0x4000);
+        symbols.insert("KBD".to_string(), 0x6000);
+        SymbolTable {
+            symbols,
+            next_address: 16,
+        }
+    }
+}
+
+#[derive(Debug, Default)]
+struct Context {
+    symbol_table: SymbolTable,
+}
+
+impl Context {
+    fn assemble(&mut self, content: &str) -> Vec<Instruction> {
+        let commands = self.parse_file(content);
+        // dbg!(&commands);
+        commands.iter().map(|c| Instruction::from(c)).collect()
+    }
+
+    fn parse_file(&mut self, content: &str) -> Vec<Command> {
+        let code_lines = content.lines()
+            .map(|line| {
+                if let Some(comment_idx) = line.find("//") {
+                    line[..comment_idx].trim()
+                } else {
+                    line.trim()
+                }
+            })
+            .filter(|x| !x.is_empty())
+            .map(|x| Self::parse_line(x))
+
+            .collect::<Vec<_>>()
+            ;
+        let mut line_number = 0;
+        for line in code_lines.iter() {
+            match line {
+                CodeLine::Label(label) => {
+                    self.symbol_table.insert(label.clone(), line_number as u16);
+                }
+                _ => {
+                    line_number += 1;
                 }
             }
-            comp = &c[1..];
         }
-        if let Some(idx) = comp.find(';') {
-            let (c, j) = comp.split_at(idx);
-            comp = c;
-            jump = j[1..].parse().unwrap_or_else(|e| panic!("Invalid jump {}: {:?}", j, e));
-        }
-        let comp = comp.parse().unwrap_or_else(|e| panic!("Invalid comp {}: {:?}", comp, e));
-        Command::C {
-            comp,
-            dest,
-            jump,
+
+        code_lines.into_iter().filter_map(|line| {
+            match line {
+                CodeLine::A(Address::Variable(symbol)) => {
+                    let address = self.symbol_table.get_or_insert(symbol.clone());
+                    Some(Command::A(address))
+                }
+                CodeLine::A(Address::Value(address)) => {
+                    Some(Command::A(address))
+                }
+                CodeLine::C { comp, dest, jump } => {
+                    Some(Command::C { comp, dest, jump })
+                }
+                _ => None
+            }
+        }).collect()
+
+    }
+
+    fn parse_line(line: &str) -> CodeLine {
+        if line.as_bytes()[0] == b'(' {
+            CodeLine::Label(line[1..line.len() - 1].to_string())
+        } else if line.as_bytes()[0] == b'@' {
+            let value = line[1..].parse().map(Address::Value)
+                .unwrap_or_else(|_| Address::Variable(line[1..].to_string()));
+            CodeLine::A(value)
+        } else {
+            let mut dest = Dest { a: false, m: false, d: false };
+            let mut jump = Jump::Null;
+            let mut comp = &line[..];
+            if let Some(idx) = line.find('=') {
+                let (d, c) = line.split_at(idx);
+                for c in d.chars() {
+                    match c {
+                        'A' => dest.a = true,
+                        'M' => dest.m = true,
+                        'D' => dest.d = true,
+                        _ => panic!("Invalid dest {}", d)
+                    }
+                }
+                comp = &c[1..];
+            }
+            if let Some(idx) = comp.find(';') {
+                let (c, j) = comp.split_at(idx);
+                comp = c;
+                jump = j[1..].parse().unwrap_or_else(|e| panic!("Invalid jump {}: {:?}", j, e));
+            }
+            let comp = comp.parse().unwrap_or_else(|e| panic!("Invalid comp {}: {:?}", comp, e));
+            CodeLine::C {
+                comp,
+                dest,
+                jump,
+            }
         }
     }
 }
@@ -199,7 +316,7 @@ fn parse_line(line: &str) -> Command {
 fn main() {
     let file_name = env::args().nth(1).expect("No file name provided");
     let file = std::fs::read_to_string(file_name).expect("Could not read file");
-    let instructions = assemble(&file);
+    let instructions = Context::default().assemble(&file);
     for instruction in instructions {
         println!("{}", instruction);
     }
@@ -208,7 +325,6 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::*;
-
 
     macro_rules! get_test_files {
         ($name:literal, $l:literal) => {
@@ -230,7 +346,7 @@ mod tests {
             let (input, expected) = get_test_files!($name, $l);
             let expected = expected.lines().collect::<Vec<_>>();
 
-            let instructions = assemble(input);
+            let instructions = Context::default().assemble(input);
             let instructions = instructions.iter().map(|x| x.to_string()).collect::<Vec<_>>();
             assert_eq!(instructions, expected);
         };
@@ -254,5 +370,20 @@ mod tests {
     #[test]
     fn pong_l() {
         test_program!("Pong", "L");
+    }
+
+    #[test]
+    fn max() {
+        test_program!("Max", "");
+    }
+
+    #[test]
+    fn rect() {
+        test_program!("Rect", "");
+    }
+
+    #[test]
+    fn pong() {
+        test_program!("Pong", "");
     }
 }
